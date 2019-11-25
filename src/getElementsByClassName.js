b54e50e38ca4b4c85228fee4c7138dda74862527
FoozBarakat <56113235+FoozBarakat@users.noreply.github.com>
2019-11-25 01:49:42 +0100

    recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,14 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
-
-// But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, element) {
+  
+	element = element || document.body;
+    var results = [];
+  
+    if (element.classList && _.contains(element.classList, className)) {
+      results.push(element);
+    }
+    _.each(element.childNodes, function(node, i) {
+      results = results.concat(getElementsByClassName(className, node));
+    });
+  
+    return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,43 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(coll) {
+if (typeof coll === "string") {
+	 	return '"' + coll + '"';
+	 } else if (typeof coll === "number" || typeof coll === "boolean") {
+	 	return "" + coll + "";
+	 } else if (coll === null) {
+	 	return 'null';
+	 } else if (Array.isArray(coll)) {
+	 	var newArr = [];
+
+	 	if (coll.length === 0) {
+	 		return '[' + coll + ']';
+	 	}
+
+	 	for(var index in coll) {
+	 		coll[index] = stringifyJSON(coll[index]);
+	 		newArr.push(coll[index]);
+	 	}
+
+	 	return '[' + newArr + ']' ;
+	 } else {
+	 	var newObj = "";
+	 	var keys = Object.keys(coll);
+
+	 	if (keys.length === 0) {
+	 		return '{}';
+	 	}
+
+	 	for (var key in coll) {
+	 		if (typeof coll[key] !== 'function' && coll[key] !== undefined) {
+	 		var newKey = stringifyJSON(key);
+	 		newObj = newObj + newKey + ":" + stringifyJSON(coll[key]) + ",";
+	 		}
+	 		
+	 	}
+	 	var final = newObj.slice(0,newObj.length - 1);
+	 	return '{' + final + '}';
+	 }
 };
+
